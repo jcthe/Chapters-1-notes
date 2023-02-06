7a410ca4a4cd0392a9a06cea150d3bc9e5342373
8226 <8226@fresnou.org>
2023-02-06 09:44:50 -0800

    Commit

---
+++ opeations.js
@@ -0,0 +1,28 @@
+//Unary operators use only one value
+console.log(typeof 15);
+console.log(typeof "this string");
+console.log(typeof true);
+console.log(typeof false);
+
+//Binary Operators use two values
+// Comparison Operators
+console.log(10 > 5);  //greater than
+
+console.log(10 < 5); //less than
+
+console.log(10 >= 5); //greater than or equal to
+
+console.log(10 <= 5); //less than or equal to
+
+console.log(10 == 5); //are they both equal? false
+
+console.log(10 == 10); //are they both equal? true
+
+console.log(10 != 5); //are they different? true
+
+console.log(10 != 10); //are they different? false
+
+// Logical
+
+//Ternary Operators use three values
+console.log(17+2);
